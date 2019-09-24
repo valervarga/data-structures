91b7bbf21897f54d3c28927ce829ec9c5efbab0c
Valer Varga <v.varga@vegaitsourcing.rs>
2019-09-24 12:10:59 +0200

    add enqueue and dequeue methods

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-/**
- * Stack is a LIFO data structure (abstract data type)
- * LIFO => Last In First Out
- */
-
 class Node {
 	constructor(val) {
 		this.val = val;
 		this.next = null;
 	}
 };
+
+/**
+ * Stack is a LIFO data structure (abstract data type)
+ * LIFO => Last In First Out
+ */
 
 class Stack {
 	constructor() {
@@ -53,4 +53,50 @@ stack.pop();
 stack.pop();
 stack.pop();
 
-console.log(stack);
+/**
+ * Queue is a FIFO data structure (abstract data type)
+ * FIFO => First In First Out
+ */
+
+class Queue {
+	constructor() {
+		this.first = null;
+		this.last = null;
+		this.length = 0;
+	}
+
+	enqueue(val) {
+		const newNode = new Node(val);
+
+		if (!this.first) {
+			this.first = newNode;
+			this.last = newNode;
+		} else {
+			this.last.next = newNode;
+			this.last = newNode;
+		}
+
+		return ++this.length;
+	}
+
+	dequeue() {
+		if (!this.first) return null;
+		const oldNode = this.first;
+
+		if (this.length === 1) {
+			this.last = null;
+		}
+		this.first = this.first.next;
+
+		this.length--;
+		return oldNode;
+	}
+}
+
+const queue = new Queue();
+queue.enqueue(1);
+queue.enqueue(230);
+queue.enqueue(56);
+queue.dequeue();
+queue.dequeue();
+queue.dequeue();
